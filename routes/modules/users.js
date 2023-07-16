341ab2d87b977954976a04ef43dc69868b41b767
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-07-16 12:56:44 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+
+// 登錄頁
 router.get('/login', (req, res) => {
   res.render('login')
 })
+// 登錄路由
 router.post('/login', (req, res) => {
 })
+// 註冊頁
 router.get('/register', (req, res) => {
   res.render('register')
+})
+// 註冊路由
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      res.render('register', { name, email, password, confirmPassword })
+    } else {
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+    .catch(err => console.log(err))
 })
 module.exports = router

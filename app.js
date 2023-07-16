1acb70277aacece4baf018c1ef5f13c1deb9176b
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-07-16 13:15:11 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+const session = require('express-session')
 require('./config/mongoose')
 
 // const restaurantList = require('./models/restaurant')
@@ -16,6 +17,11 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 

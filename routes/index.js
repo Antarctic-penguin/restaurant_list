8f432e19e8d095d871e0ebbe56a211d4509fed4c
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-07-16 12:11:38 +0800

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurant')
 const users = require('./modules/users')
 // 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/users', users)
+router.use('/', home)
 // 匯出路由器
 module.exports = router
